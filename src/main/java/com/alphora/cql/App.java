a82b04dc217cf14d1c8842ed0dd00319f6ea734d
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-11-15 20:03:09 -0700

    Update to 11 version of Java plugin

--- src/main/java/com/alphora/cql/App.java
+++ src/main/java/com/alphora/cql/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("viewer"), 1600, 900);
+        scene = new Scene(loadFXML("viewer"), 800, 600);
         stage.setScene(scene);
         stage.show();
     }
